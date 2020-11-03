86b84932048a0fea8f6837f05f2f2f965900eeba
usafder <36886437+usafder@users.noreply.github.com>
2020-11-03 19:16:00 +0500

    Added Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Card = (props) => {
+  const renderTitle = () => (
+    <h1 className="f4" style={props.titleStyle}>
+      {props.title}
+    </h1>
+  );
+
+  const renderImage = () => (
+    props.imageSource && (
+      <img
+        src={props.imageSource}
+        className="br-100 mt2 bg-light-yellow ba b--black-10"
+        style={props.imageStyle}
+        alt="pokemon"
+      />
+    )
+  );
+
+  const renderContent = () => (
+    props.children ? props.children : <p style={props.textContentStyle}>{props.textContent}</p>
+  );
+
+  return (
+    <div className="fl w-100 w-third-ns pa2" style={props.containerStyle}>
+      <div
+        className="tc pb3 br2 bg-white shadow-5 pointer grow center w-75-l"
+        style={props.contentContainerStyle}
+      >
+        {renderImage()}
+        <div>
+          {renderTitle()}
+          {renderContent()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
