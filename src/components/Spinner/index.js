ed2cea0fa23e20f6c347d29b0b006d4fd54c6f06
usafder <36886437+usafder@users.noreply.github.com>
2020-11-04 17:06:02 +0500

    Added conditional rendering for Spinner's loading text

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -4,7 +4,7 @@ import './index.css';
 const Spinner = (props) => (
   <div className="absolute spinner-container" style={props.spinnerContainerStyle}>
     <div className="br-100 ba bw4 h3 w3 bg-dark-gray spinner" style={props.spinnerStyle}></div>
-    <p style={props.loadingTextStyle}>{props.loadingText}</p>
+    {props.loadingText && <p style={props.loadingTextStyle}>{props.loadingText}</p>}
   </div>
 );
 
