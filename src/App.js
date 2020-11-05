2aab728c00fe373eac8a5914c3fecc536474621e
usafder <36886437+usafder@users.noreply.github.com>
2020-11-06 01:50:02 +0500

    Added initial code in App.js to fetch list of pokemon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import React from 'react';
+import apiClient from './services/apiClient';
+import { List, Spinner } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+// TODO: convert App component into a functional component
+class App extends React.Component {
+  state = { pokemonList: [], isLoading: true };
+
+  getPokemonList = () => {
+    apiClient.get('https://pokeapi.co/api/v2/pokemon?limit=9')
+      .then((response) => {
+        Promise.all(response.results.map((item) => apiClient.get(item.url)))
+          .then((data) => this.setState({ pokemonList: data, isLoading: false }));
+      })
+      .catch((error) => alert(error));
+  };
+
+  componentDidMount() {
+    this.getPokemonList();
+  }
+
+  renderSpinner = () => (<Spinner loadingText="Loading..." />);
+
+  renderList = () => (<List dataSource={this.state.pokemonList} />);
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoading ? this.renderSpinner() : this.renderList()}
+      </div>
+    );
+  }
 }
 
 export default App;
