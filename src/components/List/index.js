a740fe75ff147df8620269c31a42e73c63d1e908
usafder <36886437+usafder@users.noreply.github.com>
2020-11-10 23:58:26 +0500

    Code refactoring

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import { Card } from '../';
-import { getClassNameForType, padString } from '../../utils';
 
 const List = (props) => {
   return (
-    <div className="flex flex-wrap justify-around">
-      {
-        props.dataSource.map((data) => (
-          <Card
-            key={data.name + data.id}
-            title={data.name}
-            subtitle={`#${padString(data.id)}`}
-            imageSource={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${padString(data.id)}.png`}
-          >
-            {
-              // nested .map(...) not an issue since types is at most 2 in length
-              data.types.map((item, index) => (
-                <span
-                  key={item.type.name + item.id}
-                  className={`f6 dib mv2 ph2 pv1 br2 ${getClassNameForType(item.type.name)} ${index > 0 ? 'ml1' : ''}`}
-                >
-                  {item.type.name}
-                </span>
-              ))
-            }
-          </Card>
-        ))
-      }
+    <div className="flex flex-wrap justify-around" style={props.listContainerStyle}>
+      {props.dataSource.map((data, index) => props.renderItem(data, index))}
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { List, Spinner } from '../components';
+import { Card, List, Spinner } from '../components';
 import { fetchPokemonList } from '../core/thunks/pokemon';
+import { getClassNameForType, padString } from '../utils';
 
 // TODO: convert class component into a functional component
 class App extends React.Component {
@@ -11,7 +12,31 @@ class App extends React.Component {
 
   renderSpinner = () => (<Spinner loadingText="Loading..." />);
 
-  renderList = () => (<List dataSource={this.props.pokemonList} />);
+  renderItem = (data) => {
+    const id = padString(data.id);
+
+    return (
+      <Card
+        key={data.name + id}
+        title={data.name}
+        subtitle={`#${id}`}
+        imageSource={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`}
+      >
+        {
+          data.types.map(({ type }, index) => (
+            <span
+              key={type.name + id}
+              className={`f6 dib mv2 ph2 pv1 br2 ${getClassNameForType(type.name)} ${index > 0 ? 'ml1' : ''}`}
+            >
+              {type.name}
+            </span>
+          ))
+        }
+      </Card>
+    );
+  };
+
+  renderList = () => (<List dataSource={this.props.pokemonList} renderItem={this.renderItem} />);
 
   render() {
     return (
