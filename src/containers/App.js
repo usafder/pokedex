5898e6ad35d22f01beb0ca7b5a52aa29eb093904
usafder <36886437+usafder@users.noreply.github.com>
2020-11-16 22:33:00 +0500

    Updated App.js into a functional component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Badge, Card, List, Popup, Spinner } from '../components';
 import { showPopup, hidePopup } from '../core/actionCreators/popup';
 import { fetchPokemonList } from '../core/thunks/pokemon';
 import { getClassNameForType, padString } from '../utils';
 
-// TODO: convert class component into a functional component
-class App extends React.Component {
-  componentDidMount() {
-    this.props.fetchPokemonList();
-  }
+function App() {
+  const dispatch = useDispatch();
+  const dispatchShowPopup = () => dispatch(showPopup());
+  const dispatchHidePopup = () => dispatch(hidePopup());
 
-  renderSpinner = () => (<Spinner loadingText="Loading..." />);
+  const pokemonList = useSelector((state) => state.pokemon.list);
+  const isListLoading = useSelector((state) => state.pokemon.isLoading);
+  const isPopupVisible = useSelector((state) => state.popup.isVisible);
 
-  renderItem = (data) => {
+  useEffect(() => {
+    dispatch(fetchPokemonList());
+  }, [dispatch]);
+
+  const renderSpinner = () => (<Spinner loadingText="Loading..." />);
+
+  const renderItem = (data) => {
     const id = padString(data.id);
 
     return (
@@ -22,7 +29,7 @@ class App extends React.Component {
         title={data.name}
         subtitle={`#${id}`}
         imageSource={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`}
-        onClickHandler={this.props.showPopup}
+        onClickHandler={dispatchShowPopup}
       >
         <div className="flex flex-wrap justify-center pb1">
           {
@@ -39,30 +46,20 @@ class App extends React.Component {
     );
   };
 
-  renderList = () => (<List dataSource={this.props.pokemonList} renderItem={this.renderItem} />);
+  const renderList = () => (<List dataSource={pokemonList} renderItem={renderItem} />);
 
-  render() {
-    return (
-      <div>
-        {
-          this.props.isPopupVisible && (
-            <Popup hidePopup={this.props.hidePopup}>
-              <p>Work in progress.</p>
-            </Popup>
-          )
-        }
-        {this.props.isLoading ? this.renderSpinner() : this.renderList()}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {
+        isPopupVisible && (
+          <Popup hidePopup={dispatchHidePopup}>
+            <p>Work in progress.</p>
+          </Popup>
+        )
+      }
+      {isListLoading ? renderSpinner() : renderList()}
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  pokemonList: state.pokemon.list,
-  isLoading: state.pokemon.isLoading,
-  isPopupVisible: state.popup.isVisible,
-});
-
-const mapDispatchToProps = { fetchPokemonList, showPopup, hidePopup };
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
