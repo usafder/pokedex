69e8b11debf23144d9ae39b57fb56283ceb3ee90
usafder <36886437+usafder@users.noreply.github.com>
2020-11-28 03:05:08 +0500

    Updated Popup component

--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -12,6 +12,8 @@ const Popup = (props) => {
       isOpen={props.isVisible}
       onAfterOpen={handleAfterOpen}
       onAfterClose={handleAfterClose}
+      className="absolute top-0 left-0 bottom-0 right-0 top-2-ns left-2-ns bottom-2-ns right-2-ns
+        w-70-m w-50-l center-ns overflow-y-auto bg-washed-yellow br2"
     >
       {props.children}
     </Modal>
