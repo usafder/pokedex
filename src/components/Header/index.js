6dc6beab57508b4ba936cffabf2a96c84260eb3e
usafder <36886437+usafder@users.noreply.github.com>
2020-11-28 16:22:29 +0500

    Added github repo url and updated Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GITHUB_REPO_URL } from '../../constants/urls';
 
 // TODO: update About href
 const Header = () => (
@@ -6,11 +7,11 @@ const Header = () => (
     <div className="bg-near-black">
       <nav className="flex flex-wrap items-center justify-between pa3">
         <a href="/" className="ph2 ml2-m ml3-l f3 fw6 near-white no-underline ba bw2 br4">
-          Pokedex
+          Pokédex
         </a>
         <a
           className="mr2-m mr3-l f6 fw4 near-white no-underline dim"
-          href="/"
+          href={GITHUB_REPO_URL}
           target="blank"
         >
           About
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,3 @@
 export const POKE_API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 export const POKE_IMG_BASE_URL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail';
+export const GITHUB_REPO_URL = 'https://github.com/usafder/pokedex';
