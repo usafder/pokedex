f1d86fe32854cefb87c0c4f782e7d7e33e0b4bb5
usafder <36886437+usafder@users.noreply.github.com>
2020-12-07 19:30:19 +0500

    Updated Spinner component

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './index.css';
 
 const Spinner = (props) => (
-  <div className="absolute spinner-container" style={props.spinnerContainerStyle}>
-    <div className="br-100 ba bw4 h3 w3 bg-dark-gray spinner" style={props.spinnerStyle}></div>
+  <div
+    className="fixed w-100 h-100 flex flex-column items-center justify-center"
+    style={props.spinnerContainerStyle}
+  >
+    <div className="br-100 ba b--dark-red bw4 h3 w3 bg-dark-gray spinner" style={props.spinnerStyle}></div>
     {props.loadingText && <p style={props.loadingTextStyle}>{props.loadingText}</p>}
   </div>
 );
