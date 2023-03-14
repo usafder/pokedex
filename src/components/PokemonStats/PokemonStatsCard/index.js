9f390c531a881afe2f4de8a4c75ffe44fb59989a
Usman <36886437+usafder@users.noreply.github.com>
2023-03-14 17:04:41 +0500

    Created new PokemonStatsCard component

---
+++ src/components/PokemonStats/PokemonStatsCard/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../../Card';
+
+const PokemonStatsCard = ({ title, fields }) => (
+  <Card title={title} >
+    <ul className="bg-white list ma0 pl0">
+      {
+        fields.map((field) => (
+          <li key={field.label} className="pa2">
+            <span className="fw6">{field.label}:</span> {field.value}
+          </li>
+        ))
+      }
+    </ul>
+  </Card>
+);
+
+PokemonStatsCard.propTypes = {
+  title: PropTypes.string,
+  fields: PropTypes.array,
+};
+
+export default PokemonStatsCard;
