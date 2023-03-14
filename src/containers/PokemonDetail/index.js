27b8831a63fbc7bbec087ea31c4174554c5849fe
Usman <36886437+usafder@users.noreply.github.com>
2023-03-14 15:57:36 +0500

    REFACTOR: Removed renderCloseButton, renderImage, renderNameAndID methods

--- src/containers/PokemonDetail/index.js
+++ src/containers/PokemonDetail/index.js
@@ -12,30 +12,6 @@ const PokemonDetail = () => {
   const dispatch = useDispatch();
   const dispatchHidePopup = () => dispatch(hidePopup());
 
-  const renderCloseButton = () => (
-    <button
-      className="absolute top-0 right-0 mt2 mr2 fw6 ba br-100 b--black-10 bg-dark-gray near-white dim pointer"
-      onClick={dispatchHidePopup}
-    >
-      &times;
-    </button>
-  );
-
-  const renderImage = () => (
-    <img
-      src={`${process.env.REACT_APP_POKE_IMG_BASE_URL}/${id}.png`}
-      alt="pokemon"
-      className="h5-l w5-l bg-near-white br-100 shadow-1"
-    />
-  );
-
-  const renderNameAndID = () => (
-    <h1 className="code ma0 f2">
-      {selectedPokemon.name}
-      <span className="courier f5 db">#{id}</span>
-    </h1>
-  );
-
   const renderFieldsGroup = (title, fields) => (
     <Card title={title} >
       <ul className="bg-white list ma0 pl0">
@@ -73,10 +49,26 @@ const PokemonDetail = () => {
 
   return (
     <div className="tc ttc ph2 pt3">
-      {renderCloseButton()}
-      {renderImage()}
-      {renderNameAndID()}
+      <button
+        className="absolute top-0 right-0 mt2 mr2 fw6 ba br-100 b--black-10 bg-dark-gray near-white dim pointer"
+        onClick={dispatchHidePopup}
+      >
+        &times;
+      </button>
+
+      <img
+        src={`${process.env.REACT_APP_POKE_IMG_BASE_URL}/${id}.png`}
+        alt="pokemon"
+        className="h5-l w5-l bg-near-white br-100 shadow-1"
+      />
+
+      <h1 className="code ma0 f2">
+        {selectedPokemon.name}
+        <span className="courier f5 db">#{id}</span>
+      </h1>
+
       <PokemonType />
+
       {renderOtherDetails()}
     </div>
   );
