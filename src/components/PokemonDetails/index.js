e18e35c84b5d6b212e8196df97006ecc8b20d2e9
Usman <36886437+usafder@users.noreply.github.com>
2023-03-15 13:36:27 +0500

    REFACTOR: Shifted close button inside Popup

--- src/components/PokemonDetails/index.js
+++ src/components/PokemonDetails/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { useAtomValue, useSetAtom } from 'jotai';
-import { isPopupVisibleAtom, selectedPokemonAtom } from 'state/atoms';
+import { useAtomValue } from 'jotai';
+import { selectedPokemonAtom } from 'state/atoms';
 import { padString } from 'utils';
 import PokemonType from './PokemonType';
 import PokemonStats from './PokemonStats';
 
 const PokemonDetails = () => {
   const selectedPokemon = useAtomValue(selectedPokemonAtom);
-  const setIsPopupVisible = useSetAtom(isPopupVisibleAtom);
   const selectedPokemonId = padString(selectedPokemon.id);
-  const hidePopup = () => setIsPopupVisible(false);
 
   return (
     <div className="tc ttc ph2 pt3">
-      <button
-        className="absolute top-0 right-0 mt2 mr2 fw6 ba br-100 b--black-10 bg-dark-gray near-white dim pointer"
-        onClick={hidePopup}
-      >
-        &times;
-      </button>
-
       <img
         src={`${process.env.REACT_APP_POKE_IMG_BASE_URL}/${selectedPokemonId}.png`}
         alt="pokemon"
--- src/components/Popup/index.js
+++ src/components/Popup/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Modal from 'react-modal';
 import PropTypes from 'prop-types';
-import { useAtomValue } from 'jotai';
+import { useAtom } from 'jotai';
 import { isPopupVisibleAtom } from 'state/atoms';
 
 Modal.setAppElement('#root');
 
 const Popup = (props) => {
+  const [isPopupVisible, setIsPopupVisible] = useAtom(isPopupVisibleAtom);
+  const hidePopup = () => setIsPopupVisible(false);
   const handleAfterOpen = () => document.body.style.overflow = 'hidden';
   const handleAfterClose = () => document.body.style.overflow = 'auto';
-  const isPopupVisible = useAtomValue(isPopupVisibleAtom);
 
   return (
     <Modal
@@ -19,6 +20,12 @@ const Popup = (props) => {
       className="absolute top-0 left-0 bottom-0 right-0 top-2-ns left-2-ns bottom-2-ns right-2-ns
         w-70-m w-50-l center-ns overflow-y-auto bg-washed-yellow br2"
     >
+      <button
+        className="absolute top-0 right-0 mt2 mr2 fw6 ba br-100 b--black-10 bg-dark-gray near-white dim pointer"
+        onClick={hidePopup}
+      >
+        &times;
+      </button>
       {props.children}
     </Modal>
   );
