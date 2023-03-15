4a8076eee6b32c8371034769f95441d4a6e8e66d
Usman <36886437+usafder@users.noreply.github.com>
2023-03-15 21:48:32 +0500

    CHORE: Removed url from header title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,9 @@ const Header = () => (
   <header>
     <div className="bg-near-black">
       <nav className="flex flex-wrap items-center justify-between pa3">
-        <a href="/" className="ph2 ml2-m ml3-l f3 fw6 near-white no-underline ba bw2 br4">
+        <span className="ph2 ml2-m ml3-l f3 fw6 near-white no-underline ba bw2 br4">
           Pokédex
-        </a>
+        </span>
         <a
           className="mr2-m mr3-l f6 fw4 near-white no-underline dim"
           href={process.env.REACT_APP_GITHUB_REPO_URL}
